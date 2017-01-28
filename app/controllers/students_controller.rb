401777790588083f2e9effa125f7940875a659fd
vagrant <vagrant@precise32.(none)>
2017-01-28 17:22:40 +0000

    Created an edit student info function

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,30 +1,48 @@
 class StudentsController < ApplicationController
-  def index
-  	@students = Student.order(:name)
-  end
+	def index
+  		@students = Student.order(:name)
+	end
 
-  def show
-  	@student = Student.find(params[:id])
-  end
+	def show
+  		@student = find_student
+	end
 
-  def new
-  	@student = Student.new
-  end
+	def new
+  		@student = Student.new
+	end
 
-  def create
-  	@student = Student.new(student_params)
+	def create
+  		@student = Student.new(student_params)
 
-  	if @student.save # If the validation pass
-  		redirect_to @student
+		if @student.save # If the validation pass
+  			redirect_to @student
 
-  	 else
-  	 	render :new 
-  	 end
-  end
+  	 	else
+  	 		render :new 
+  		end
+	end
+
+	def edit
+		@student = find_student
+	end
+
+	def update
+		@student = find_student
+
+		if @student.update(student_params)
+			redirect_to @student 
+		else
+			render :edit
+		end
+	end
 
 	private
-  
-   def student_params
+  	
+  	def find_student
+  		@student = Student.find(params[:id])
+  	end
+
+	def student_params
       params.require(:student).permit(:name)
     end
 end
